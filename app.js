e1a52bfbf7287c69102cbde8d9252a246ee9c33b
joriskian <jo_risk@hotmail.com>
2020-06-18 22:30:28 +0200

    now with three

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+
 class Timer extends React.Component{
     constructor(props){
         super(props)
@@ -21,7 +22,7 @@ class Timer extends React.Component{
     }
 }
 
-class DrawingCanvas extends React.Component{
+class SimpleCanvas extends React.Component{
     componentDidMount() {
         //console.log(this.refs.canvas)
         const canvas = this.refs.canvas
@@ -62,11 +63,9 @@ class P5Canvas extends React.Component{
     Sketch = (p)=>{
         let x = 20
         let y = 2
+        
         p.setup = ()=>{
             p.createCanvas(200, 200);
-            console.log('color : '+this.state.color)
-            console.log('strokeColor : '+this.state.strokeColor)
-
         }
         p.draw = ()=>{
             p.background(0);
@@ -82,20 +81,56 @@ class P5Canvas extends React.Component{
     render(){
         return <div id = 'canvas'>
             <button onClick = {this.randomColor}>random color</button>
-            <div ref ='this.myRef' color = {this.state.color}></div>
+            <div ref ={this.myRef} color = {this.state.color}></div>
         </div>
     }
 }
-function Bonjour(props){
+class THREECanvas extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount() {
+      var scene = new THREE.Scene();
+      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+      var renderer = new THREE.WebGLRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      // document.body.appendChild( renderer.domElement );
+      // use ref as a mount point of the Three.js scene instead of the document.body
+      this.mount.appendChild( renderer.domElement );
+      var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+      var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+      var cube = new THREE.Mesh( geometry, material );
+      scene.add( cube );
+      camera.position.z = 5;
+      var animate = function () {
+        requestAnimationFrame( animate );
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render( scene, camera );
+      };
+      animate();
+    }
+    render() {
+      return (
+          <div> this is three.js
+              <div ref={ref => (this.mount = ref)} />
+          </div>
+      )
+    }
+  }
+
+function MyApp(props){
 
     return <div>
         <P5Canvas color={150,10,10} strokeColor ={125} />
         Bonjour tous le monde il est : <Timer start = {0}/>
-        <DrawingCanvas/>
+        <SimpleCanvas/>
         <P5Canvas color={10,150,10} strokeColor = {255}/>
+        <THREECanvas/>
     </div>
 }
 
 const rootElement = document.getElementById('app')
 
-ReactDOM.render(<Bonjour></Bonjour>, rootElement );
+ReactDOM.render(<MyApp></MyApp>, rootElement );
